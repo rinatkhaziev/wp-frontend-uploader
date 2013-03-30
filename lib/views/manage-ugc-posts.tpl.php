dcad7f0f48b35d7f863e6148982a33649d35f816
Rinat Khaziev <rinat.khaziev@gmail.com>
2013-03-29 20:20:00 -0500

    Files for previous commit

---
+++ lib/views/manage-ugc-posts.tpl.php
@@ -0,0 +1,85 @@
+<?php
+$title = __( 'Manage UGC Posts', 'frontend-uploader' );
+set_current_screen( 'post' );
+if ( ! current_user_can( 'publish_posts' ) )
+	wp_die( __( 'You do not have permission to publish posts.', 'frontend-uploader' ) );
+
+$wp_post_list_table = new FU_WP_Posts_List_Table();
+$pagenum = $wp_post_list_table->get_pagenum();
+$doaction = $wp_post_list_table->current_action();
+$wp_post_list_table->prepare_items();
+wp_enqueue_script( 'wp-ajax-response' );
+wp_enqueue_script( 'jquery-ui-draggable' );
+wp_enqueue_script( 'media' );
+
+?>
+
+<div class="wrap">
+<?php screen_icon(); ?>
+<h2><?php echo esc_html( $title ); ?> <a href="posts-new.php" class="add-new-h2"><?php echo esc_html_x( 'Add New', 'file' ); ?></a> <?php
+if ( isset( $_REQUEST['s'] ) && $_REQUEST['s'] )
+	printf( '<span class="subtitle">' . __( 'Search results for &#8220;%s&#8221;', 'frontend-uploader' ) . '</span>', get_search_query() ); ?>
+</h2>
+
+<?php
+$message = '';
+if ( isset( $_GET['posted'] ) && (int) $_GET['posted'] ) {
+	$message = __( 'Media attachment updated.', 'frontend-uploader' );
+	$_SERVER['REQUEST_URI'] = remove_query_arg( array( 'posted' ), $_SERVER['REQUEST_URI'] );
+}
+
+if ( isset( $_GET['attached'] ) && (int) $_GET['attached'] ) {
+	$attached = (int) $_GET['attached'];
+	$message = sprintf( _n( 'Reattached %d attachment.', 'Reattached %d attachments.', $attached ), $attached );
+	$_SERVER['REQUEST_URI'] = remove_query_arg( array( 'attached' ), $_SERVER['REQUEST_URI'] );
+}
+
+if ( isset( $_GET['deleted'] ) && (int) $_GET['deleted'] ) {
+	$message = sprintf( _n( 'Media attachment permanently deleted.', '%d media attachments permanently deleted.', $_GET['deleted'] ), number_format_i18n( $_GET['deleted'] ) );
+	$_SERVER['REQUEST_URI'] = remove_query_arg( array( 'deleted' ), $_SERVER['REQUEST_URI'] );
+}
+
+if ( isset( $_GET['trashed'] ) && (int) $_GET['trashed'] ) {
+	$message = sprintf( _n( 'Media attachment moved to the trash.', '%d media attachments moved to the trash.', $_GET['trashed'] ), number_format_i18n( $_GET['trashed'] ) );
+	$message .= ' <a href="' . esc_url( wp_nonce_url( 'upload.php?doaction=undo&action=untrash&ids='.( isset( $_GET['ids'] ) ? $_GET['ids'] : '' ), "bulk-media" ) ) . '">' . __( 'Undo', 'frontend-uploader' ) . '</a>';
+	$_SERVER['REQUEST_URI'] = remove_query_arg( array( 'trashed' ), $_SERVER['REQUEST_URI'] );
+}
+
+if ( isset( $_GET['untrashed'] ) && (int) $_GET['untrashed'] ) {
+	$message = sprintf( _n( 'Media attachment restored from the trash.', '%d media attachments restored from the trash.', $_GET['untrashed'] ), number_format_i18n( $_GET['untrashed'] ) );
+	$_SERVER['REQUEST_URI'] = remove_query_arg( array( 'untrashed' ), $_SERVER['REQUEST_URI'] );
+}
+
+if ( isset( $_GET['approved'] ) ) {
+	$message = 'The post was approved';
+}
+
+$messages[1] = __( 'Media attachment updated.', 'frontend-uploader' );
+$messages[2] = __( 'Media permanently deleted.', 'frontend-uploader' );
+$messages[3] = __( 'Error saving media attachment.', 'frontend-uploader' );
+$messages[4] = __( 'Media moved to the trash.', 'frontend-uploader' ) . ' <a href="' . esc_url( wp_nonce_url( 'upload.php?doaction=undo&action=untrash&ids='.( isset( $_GET['ids'] ) ? $_GET['ids'] : '' ), "bulk-media" ) ) . '">' . __( 'Undo', 'frontend-uploader' ) . '</a>';
+$messages[5] = __( 'Media restored from the trash.', 'frontend-uploader' );
+
+if ( isset( $_GET['message'] ) && (int) $_GET['message'] ) {
+	$message = $messages[$_GET['message']];
+	$_SERVER['REQUEST_URI'] = remove_query_arg( array( 'message' ), $_SERVER['REQUEST_URI'] );
+}
+
+if ( !empty( $message ) ) { ?>
+<div id="message" class="updated"><p><?php echo $message; ?></p></div>
+<?php } ?>
+
+<?php $wp_post_list_table->views(); ?>
+
+<form id="posts-filter" action="" method="get">
+
+<?php $wp_post_list_table->search_box( __( 'Search Posts', 'frontend-uploader' ), 'posts' ); ?>
+
+<?php $wp_post_list_table->display(); ?>
+
+<div id="ajax-response"></div>
+<?php find_posts_div(); ?>
+<br class="clear" />
+
+</form>
+</div>
