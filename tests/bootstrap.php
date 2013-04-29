3363c909d785480c44f1fa128e7f65c82d26aef2
Rinat Khaziev <rinat.khaziev@gmail.com>
2013-04-28 21:07:20 -0500

    Updating the testsuite bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,6 @@
 <?php
 require_once getenv( 'WP_TESTS_DIR' ) . '/includes/functions.php';
 
-tests_add_filter( 'muplugins_loaded', function() {
-	require dirname( __FILE__ ) . '/../frontend-uploader.php';
-} );
+tests_add_filter( 'muplugins_loaded', create_function( '', "require dirname( __FILE__ ) . '/../frontend-uploader.php';" ) );
 
 require getenv( 'WP_TESTS_DIR' ) . '/includes/bootstrap.php';
