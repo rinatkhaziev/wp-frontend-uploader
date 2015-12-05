dd1b82fe3387bbaab0ac50ff6e2fffcf5f4df0c1
Rinat Khaziev <rinat.khaziev@gmail.com>
2015-12-05 13:29:00 -0600

    Fixing 404 for jquery validate dep

--- frontend-uploader.php
+++ frontend-uploader.php
@@ -1202,7 +1202,7 @@ class Frontend_Uploader {
 	 */
 	function enqueue_scripts() {
 		wp_enqueue_style( 'frontend-uploader', FU_URL . 'lib/css/frontend-uploader.css' );
-		wp_enqueue_script( 'jquery-validate', FU_URL .' lib/js/validate/jquery.validate.js ', array( 'jquery' ) );
+		wp_enqueue_script( 'jquery-validate', FU_URL . 'lib/js/validate/jquery.validate.js ', array( 'jquery' ) );
 		wp_enqueue_script( 'frontend-uploader-js', FU_URL . 'lib/js/frontend-uploader.js', array( 'jquery', 'jquery-validate' ) );
 		// Include localization strings for default messages of validation plugin
 		// Filter is needed for wordpress.com
