4faf7fa2dd284e646ca44ac4236a20c89b81ebe8
Rinat Khaziev <rinat.khaziev@gmail.com>
2016-03-23 16:31:52 -0500

    Second pass at cleaning up FU WP Media List table class

--- lib/php/class-frontend-uploader-wp-media-list-table.php
+++ lib/php/class-frontend-uploader-wp-media-list-table.php
@@ -14,6 +14,7 @@ class FU_WP_Media_List_Table extends WP_Media_List_Table {
 		global $lost, $wpdb, $wp_query, $post_mime_types, $avail_post_mime_types;
 
 		add_filter( 'posts_where', array( $this, 'modify_post_status_to_private' ) );
+
 		parent::prepare_items();
 
 		$this->items = $wp_query->posts;
@@ -33,37 +34,14 @@ class FU_WP_Media_List_Table extends WP_Media_List_Table {
 	function filter_media_row_actions( $actions, $post, $detached ) {
 		$user_can = current_user_can( 'edit_post', $post->ID );
 
-		$detached = $post->post_parent == 0;
-
-		if ( ! $user_can ) {
-			// Only keep "view" action
-			return array( $actions['view'] );
-		}
+		$detached = $post->post_parent === 0;
 
 		$actions['pass'] = '<a href="' . admin_url( 'admin-ajax.php' ).'?action=approve_ugc&id=' . $post->ID . '&fu_nonce=' . wp_create_nonce( FU_NONCE ). '">'. __( 'Approve', 'frontend-uploader' ) .'</a>';
 
 		if ( ! $detached ) {
-
-			$detach_url = add_query_arg( array(
-				'parent_post_id' => $post->post_parent,
-				'media[]' => $post->ID,
-				'_wpnonce' => wp_create_nonce( 'bulk-' . $this->_args['plural'] )
-			), 'upload.php?page=manage_frontend_uploader' );
-
-			$actions['detach'] = sprintf(
-				'<a href="%s" class="hide-if-no-js detach-from-parent" aria-label="%s">%s</a>',
-				$detach_url,
-				/* translators: %s: title of the post the attachment is attached to */
-				esc_attr( sprintf( __( 'Detach from &#8220;%s&#8221;' ), $title ) ),
-				__( 'Detach' )
+			$actions['re-attach'] = sprintf( '<a class="hide-if-no-js" onclick="findPosts.open( \'media[]\', \'%d\' );return false;" href="#the-list">%s</a>',
+				$post->ID, esc_html( __( 'Re-Attach', 'frontend-uploader' ) )
 			);
-
-
-			$actions['re-attach'] = sprintf( '<a class="hide-if-no-js" onclick="findPosts.open(\'media[]\', %s );return false;" href="#the-list">%s</a>',
-				wp_json_encode( $post->ID ), esc_html( __( 'Re-Attach', 'frontend-uploader' ) )
-			);
-		} else {
-
 		}
 
 		return $actions;
